94caa538425f27d167d9cc28dd04ace943c7f067
Rahul Raj <rahultime2018@gmail.com>
2025-05-15 22:15:02 +0530

    Updates

---
+++ Part 2: Write Your First Program in C++/dataTypes.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int integerData = 1;
+    float floatData = 3.14;
+    char characterData = 'a';
+    bool isTrue = true;
+    string stringData = "Rahul Raj";
+    cout<<integerData<<endl;
+    cout<<floatData<<endl;
+    cout<<characterData<<endl;
+    cout<<isTrue<<endl;
+    cout<<stringData;
+}
---
+++ Part 2: Write Your First Program in C++/dataTypes.md
@@ -0,0 +1,6 @@
+**Data Types :** If we want to store any kind of data in computer then we need to create a variable and define it's type and that type is called Data Types in C++ and that variable name is called variable in C++.
+*eg :* 
+```
+int x = 20;
+```
+here it means that x is a variable of *integer* type.
